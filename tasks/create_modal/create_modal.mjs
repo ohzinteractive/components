9cd65f42b0b2c0d571886d3e55dfe506df9e07bf
Federico Gratti <federicogratti@gmail.com>
2023-04-20 18:51:48 -0300

    Fix capitalize method on create_modal command

--- tasks/create_modal/create_modal.mjs
+++ tasks/create_modal/create_modal.mjs
@@ -246,8 +246,8 @@ class ModalCreator
 
   capitalize(string)
   {
-    let aux_string = this.capitalize_first_letter(string);
-    aux_string = this.snake_to_camelcase(aux_string);
+    let aux_string = this.snake_to_camelcase(string);
+    aux_string = this.capitalize_first_letter(aux_string);
 
     return aux_string;
   }
